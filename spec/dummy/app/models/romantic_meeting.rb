1990cf7c716e9ffed0173be0ab172794bd0e0e35
Simon Hildebrandt <simonhildebrandt@gmail.com>
2013-09-04 14:56:00 +1000

    Tweaked testing docs

--- spec/dummy/app/models/romantic_meeting.rb
+++ spec/dummy/app/models/romantic_meeting.rb
@@ -2,6 +2,6 @@ class RomanticMeeting < ActiveRecord::Base
   attr_accessible :length, :start
 
   include Chronorails::ChronicAccessors
-  chronic_field :start, :required => true, :validates => true
-  chronic_duration_field :length, :validates => true
+  chronic_field :start, :required => true
+  chronic_duration_field :length
 end
